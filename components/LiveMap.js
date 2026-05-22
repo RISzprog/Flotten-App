b154638aee8615ba7e56ffdd44ed8886c4a3be0a
RISzprog <diehl.a@risgmbh.de>
2026-05-22 13:36:54 +0200

    Create LiveMap.js

---
+++ components/LiveMap.js
@@ -0,0 +1,39 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default function LiveMap({ zeiten }) {
+  const punkte = zeiten.filter((z) => z.latitude && z.longitude);
+
+  if (punkte.length === 0) {
+    return <div className="emptyMap">Keine GPS-Daten vorhanden</div>;
+  }
+
+  const erster = punkte[0];
+
+  return (
+    <div style={{ height: "420px", width: "100%", borderRadius: "20px", overflow: "hidden" }}>
+      <MapContainer
+        center={[Number(erster.latitude), Number(erster.longitude)]}
+        zoom={12}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {punkte.map((z) => (
+          <Marker key={z.id} position={[Number(z.latitude), Number(z.longitude)]}>
+            <Popup>
+              <strong>{z.fahrzeug}</strong>
+              <br />
+              {z.mitarbeiter}
+              <br />
+              {z.beifahrer ? `Beifahrer: ${z.beifahrer}` : ""}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
+}
